80e4afe15314581e37d76a80dcf7a02b0d7b6166
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-01-31 22:28:03 +0530

    Improved player rigidbody

---
+++ Dungeon Escape/Assets/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+
+        if (_rigid == null)
+        {
+            Debug.LogError("Rigidbody not found on the Player gameobject");
+        }
+    }
+
+    void Update()
+    {
+        // horizontal input for left and right
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+    }
+}
