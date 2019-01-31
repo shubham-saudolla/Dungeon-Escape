285c078d0b4fe9dd1a55de1a0281ddc8a38a057d
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-01-31 23:07:27 +0530

    Set up player movement

--- Dungeon Escape/Assets/Assets/Scripts/Player.cs
+++ Dungeon Escape/Assets/Assets/Scripts/Player.cs
@@ -9,7 +9,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D _rigid;
+    Rigidbody2D _rigid;
+    [SerializeField]
+    float _jumpForce = 5f;
+    [SerializeField]
+    float _raycastDistance = 0.6f;
+    [SerializeField]
+    bool _grounded = false;
+    [SerializeField]
+    LayerMask _groundLayer;
+    bool _resetJumpNeeded = false;
 
     void Start()
     {
@@ -24,7 +33,34 @@ public class Player : MonoBehaviour
     void Update()
     {
         // horizontal input for left and right
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+        float move = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && _grounded == true)
+        {
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            _grounded = false;
+            _resetJumpNeeded = true;
+            StartCoroutine(ResetJumpNeededRoutine());
+        }
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, _raycastDistance, _groundLayer);
+        Debug.DrawRay(transform.position, dir: Vector2.down * _raycastDistance, Color.green);
+
+        // check whether raycast has hit a collider
+        if (hitInfo.collider != null)
+        {
+            if (_resetJumpNeeded == false)
+            {
+                _grounded = true;
+            }
+        }
+
+        _rigid.velocity = new Vector2(move, _rigid.velocity.y);
+    }
+
+    IEnumerator ResetJumpNeededRoutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        _resetJumpNeeded = false;
     }
 }
