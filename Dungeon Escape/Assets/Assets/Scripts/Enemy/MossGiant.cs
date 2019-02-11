9b800d6a20f77b63995891a0340793d1f87eeacc
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-02-11 13:09:59 +0530

    Set up enemy class shared behaviour

--- Dungeon Escape/Assets/Assets/Scripts/Enemy/MossGiant.cs
+++ Dungeon Escape/Assets/Assets/Scripts/Enemy/MossGiant.cs
@@ -9,53 +9,5 @@ using UnityEngine;
 
 public class MossGiant : Enemy
 {
-    Vector3 _currentTarget;
-    SpriteRenderer _mossGiantSprite;
-    Animator _anim;
 
-    void Start()
-    {
-        _mossGiantSprite = GetComponentInChildren<SpriteRenderer>();
-        _anim = GetComponentInChildren<Animator>();
-    }
-
-    public override void Update()
-    {
-        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-        {
-            return;
-        }
-
-        Movement();
-    }
-
-    void Movement()
-    {
-        if (_currentTarget == pointA.position)
-        {
-            _mossGiantSprite.flipX = true;
-        }
-        else
-        {
-            _mossGiantSprite.flipX = false;
-        }
-
-        if (transform.position == pointA.position)
-        {
-            _currentTarget = pointB.position;
-            _anim.SetTrigger("Idle");
-        }
-        else if (transform.position == pointB.position)
-        {
-            _currentTarget = pointA.position;
-            _anim.SetTrigger("Idle");
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
-    }
-
-    public override void Attack()
-    {
-
-    }
 }
--- Dungeon Escape/Assets/Assets/Scripts/Enemy/Spider.cs
+++ Dungeon Escape/Assets/Assets/Scripts/Enemy/Spider.cs
@@ -9,48 +9,5 @@ using UnityEngine;
 
 public class Spider : Enemy
 {
-    Vector3 _currentTarget;
-    SpriteRenderer _spiderSprite;
-    Animator _anim;
 
-    void Start()
-    {
-        _spiderSprite = GetComponentInChildren<SpriteRenderer>();
-        _anim = GetComponentInChildren<Animator>();
-    }
-
-    public override void Update()
-    {
-        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-        {
-            return;
-        }
-
-        Movement();
-    }
-
-    void Movement()
-    {
-        if (_currentTarget == pointA.position)
-        {
-            _spiderSprite.flipX = true;
-        }
-        else
-        {
-            _spiderSprite.flipX = false;
-        }
-
-        if (transform.position == pointA.position)
-        {
-            _currentTarget = pointB.position;
-            _anim.SetTrigger("Idle");
-        }
-        else if (transform.position == pointB.position)
-        {
-            _currentTarget = pointA.position;
-            _anim.SetTrigger("Idle");
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
-    }
 }
