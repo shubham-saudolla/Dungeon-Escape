2af28208656ae687efc72611d1e1f6d73323aa6b
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-02-16 11:19:37 +0530

    Implemented the IDamageable interface

--- Dungeon Escape/Assets/Assets/Scripts/Enemy/Spider.cs
+++ Dungeon Escape/Assets/Assets/Scripts/Enemy/Spider.cs
@@ -7,10 +7,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spider : Enemy
+public class Spider : Enemy, IDamageable
 {
+    public int Health { get; set; }
+
     public override void Init()
     {
         base.Init();
+    }
+
+    public void Damage()
+    {
+
     }
 }
---
+++ Dungeon Escape/Assets/Assets/Scripts/IDamageable.cs
@@ -0,0 +1,14 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageable
+{
+    int Health { get; set; }
+    void Damage();
+}
