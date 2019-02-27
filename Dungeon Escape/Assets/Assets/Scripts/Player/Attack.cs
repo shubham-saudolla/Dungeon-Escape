3df2636abfa2e67cce538bf4ce96cd6088022356
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-02-27 14:23:36 +0530

    Set up the sword hitbox

---
+++ Dungeon Escape/Assets/Assets/Scripts/Player/Attack.cs
@@ -0,0 +1,16 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Hit: " + other.name);
+    }
+}
