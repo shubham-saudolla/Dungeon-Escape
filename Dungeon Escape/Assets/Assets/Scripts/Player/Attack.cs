565a929e1f7da89b76cd5fa40d32b613bd1bd5eb
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-02-27 18:59:10 +0530

    Implemented skeleton damage and cooldown

--- Dungeon Escape/Assets/Assets/Scripts/Player/Attack.cs
+++ Dungeon Escape/Assets/Assets/Scripts/Player/Attack.cs
@@ -9,8 +9,28 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    private bool _canDamage = true;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Hit: " + other.name);
+
+        IDamageable hit = other.GetComponent<IDamageable>();
+
+        if (hit != null)
+        {
+            if (_canDamage)
+            {
+                hit.Damage();
+                _canDamage = false;
+                StartCoroutine(ResetDamage());
+            }
+        }
+    }
+
+    IEnumerator ResetDamage()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _canDamage = true;
     }
 }
