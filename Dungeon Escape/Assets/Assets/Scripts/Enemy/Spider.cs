13a7623ee5b865e6b57d98a4f5a5054d5ba131fd
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-03-01 00:06:05 +0530

    Implemented the spider damage

--- Dungeon Escape/Assets/Assets/Scripts/Enemy/Spider.cs
+++ Dungeon Escape/Assets/Assets/Scripts/Enemy/Spider.cs
@@ -7,13 +7,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spider : Enemy
+public class Spider : Enemy, IDamageable
 {
     public int Health { get; set; }
+
+    public GameObject acidEffectPrefab;
 
     public override void Init()
     {
         base.Init();
+
+        Health = base.health;
     }
 
     public override void Movement()
@@ -23,6 +27,16 @@ public class Spider : Enemy
 
     public void Damage()
     {
+        Health--;
 
+        if (Health < 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void Attack()
+    {
+        Instantiate(acidEffectPrefab, transform.position, Quaternion.identity);
     }
 }
---
+++ Dungeon Escape/Assets/Assets/Scripts/Enemy/SpiderAnimationEvent.cs
@@ -0,0 +1,23 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderAnimationEvent : MonoBehaviour
+{
+    private Spider _spider;
+
+    void Start()
+    {
+        _spider = transform.parent.GetComponent<Spider>();
+    }
+
+    public void Fire()
+    {
+        _spider.Attack();
+    }
+}
