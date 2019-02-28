fa774e6ad7aa92cc6d8e6408043783f09507f20e
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-02-28 16:06:54 +0530

    Moss giant attack complete

--- Dungeon Escape/Assets/Assets/Scripts/Enemy/MossGiant.cs
+++ Dungeon Escape/Assets/Assets/Scripts/Enemy/MossGiant.cs
@@ -14,10 +14,38 @@ public class MossGiant : Enemy, IDamageable
     public override void Init()
     {
         base.Init();
+
+        Health = base.health;
+    }
+
+    public override void Movement()
+    {
+        base.Movement();
+
+        Vector3 direction = player.transform.localPosition - transform.localPosition;
+
+        if (direction.x > 0 && anim.GetBool("InCombat") == true)
+        {
+            sprite.flipX = false;
+        }
+        else if (direction.x < 0 && anim.GetBool("InCombat") == true)
+        {
+            sprite.flipX = true;
+        }
     }
 
     public void Damage()
     {
+        Debug.Log("Moss giant damaged");
 
+        Health--;
+        anim.SetTrigger("Hit");
+        isHit = true;
+        anim.SetBool("InCombat", true);
+
+        if (Health < 1)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
