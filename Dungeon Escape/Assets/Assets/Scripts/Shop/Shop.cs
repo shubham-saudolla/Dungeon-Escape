597b9ad155524cd8adb25faf48d2ba1627944a53
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-03-01 12:32:23 +0530

    Added the basic shop behaviour

---
+++ Dungeon Escape/Assets/Assets/Scripts/Shop/Shop.cs
@@ -0,0 +1,29 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public GameObject shopPanel;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            shopPanel.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            shopPanel.SetActive(false);
+        }
+    }
+}
