d1b6c6f679f6d3bd852589701d6924687d14f98d
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-03-03 18:19:23 +0530

    Set up Unity Ads

---
+++ Dungeon Escape/Assets/Assets/Scripts/AdsManager.cs
@@ -0,0 +1,43 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour
+{
+    public void ShowRewardedAd()
+    {
+        Debug.Log("Showing rewarded ad");
+
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            var options = new ShowOptions
+            {
+                resultCallback = HandleShowResult
+            };
+
+            Advertisement.Show("rewardedVideo", options);
+        }
+    }
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                // award 100 gems to the player
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("Skipped the ad");
+                break;
+            case ShowResult.Failed:
+                Debug.Log("Failed");
+                break;
+        }
+    }
+}
